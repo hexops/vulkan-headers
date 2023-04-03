e44309e935c03d463eaf9866e43ed23d5e5bab68
LordMZTE <lord@mzte.de>
2023-04-03 17:22:46 +0200

    use Zig build system

---
+++ build.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib = b.addStaticLibrary(.{
+        .name = "vulkan-headers",
+        .root_source_file = .{ .path = "stub.c" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    inline for (.{ "vk_video", "vulkan" }) |subdir| {
+        lib.installHeadersDirectory("include/" ++ subdir, subdir);
+    }
+
+    lib.install();
+}
